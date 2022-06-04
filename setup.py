216c4e4a0e148b1267ea699af6c237c8a7c8c838
Tsche <Tsche@users.noreply.github.com>
2022-06-04 09:00:58 +0200

    init

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="palliate-codegen",
+    version="0.0.1",
+    author="Tsche",
+    author_email="contact@palliate.io",
+    description="Code generation utility",
+    url="https://github.com/palliate/codegen",
+    project_urls={
+        "Bug Tracker": "https://github.com/palliate/codegen/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=[],
+)
