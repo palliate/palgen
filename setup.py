2f6171202d05d074312267d0aa1db4fd8f77b73d
Tsche <Tsche@users.noreply.github.com>
2022-06-04 12:46:14 +0200

    package data

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    include_package_data=True,
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
