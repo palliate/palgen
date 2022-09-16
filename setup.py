d1f70e17f653788f7f9502a64215ee6de3048626
Tsche <Tsche@users.noreply.github.com>
2022-09-16 03:06:31 +0200

    empty package to reserve new name

--- setup.py
+++ setup.py
@@ -1,35 +1,21 @@
-#!/usr/bin/env python3
 import setuptools
-from pathlib import Path
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
 
 setuptools.setup(
-    name="palliate-codegen",
-    version="0.0.2",
+    name="palgen",
+    version="0.0.1",
     author="Tsche",
     author_email="contact@palliate.io",
     description="Code generation utility",
     long_description="Code generation utility",
     long_description_content_type="text/markdown",
-    url="https://github.com/palliate/codegen",
+    url="https://github.com/palliate/palgen",
     project_urls={
-        "Bug Tracker": "https://github.com/palliate/codegen/issues",
+        "Bug Tracker": "https://github.com/palliate/palgen/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    include_package_data=True,
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
-    install_requires=["jinja2", "click", "colorama", "toml"],
-    entry_points={
-        'console_scripts': [
-            'palliate-codegen=palliate_codegen:main'
-        ]
-    }
+    packages=[],
 )
--- src/palliate_codegen/__main__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from colorama import init
-init()
-
-from . import main
-
-if __name__ == "__main__":
-    main()
